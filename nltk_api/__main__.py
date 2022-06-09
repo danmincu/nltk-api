4f82a1daab6ac98c7e398e81eefb8219e8fcceae
Dan Mincu <danmincu@gmail.com>
2022-06-09 13:33:41 -0400

    Fix main entry program and the way it chooses the port

--- nltk_api/__main__.py
+++ nltk_api/__main__.py
@@ -4,12 +4,15 @@ from nltk_api.application import app
 
 
 def main():
-    port = int(os.environ['APP_PORT'])
+    port = 5000
+    try:
+        port = int(os.environ['APP_PORT'])
+    except (ValueError, KeyError):
+        print(f'Environment variable for APP_PORT is not defined. Using the default port:{port}')
 
     http_server = WSGIServer(('', port), app)
     http_server.serve_forever()
     pass
-
 
 if __name__ == '__main__':
     main()
